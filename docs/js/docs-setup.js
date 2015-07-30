30ddf02b2c4a4c1a89f38e4c986a61ed540974e2
Joy <caishaojiang@gmail.com>
2015-07-30 23:38:45 +0800

    Just copy docs folder from master branch

---
+++ docs/js/docs-setup.js
@@ -0,0 +1,28 @@
+NG_DOCS={
+  "sections": {
+    "api": "Angular-sui Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "sui.checkbox.directive:sui-checkbox",
+      "shortName": "sui-checkbox",
+      "type": "directive",
+      "moduleName": "sui.checkbox",
+      "shortDescription": "Checkbox element",
+      "keywords": "$scope aftertoggle angular api app call called checkbox checked class click cloudflare controller css ctrl data democtrl directive disabled ea element false function href html https js json label message module ng-controller on-toggle ontoggle positive rel segment slider status stylesheet sui sui-checkbox text three toggle toggled true type ui ui-style var vm"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "/api",
+  "scripts": [
+    "jquery-2.1.4.js",
+    "angular.js",
+    "angular-animate.js",
+    "sui-checkbox.js"
+  ]
+};
