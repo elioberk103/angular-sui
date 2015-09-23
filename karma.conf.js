0f1e64df0b9c94edcd90cd9a7dc90a7ce8118efa
agongdai <caishaojiang@gmail.com>
2015-09-23 12:27:43 +0800

    Exclude .spec files for testing.

--- karma.conf.js
+++ karma.conf.js
@@ -29,7 +29,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-        './src/**/!(*spec)/*.js': ['coverage']
+        './src/**/!(*spec).js': ['coverage']
     },
 
     coverageReporter: {
@@ -68,6 +68,6 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false
+    singleRun: true
   })
-}
+};
